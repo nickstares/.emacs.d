55fb019a7b1d33520ada66d1b065a52f5e57bcc1
Nick Stares <nickstares0@gmail.com>
2019-05-21 14:59:35 -0500

    Add cider, treemacs; comment out rust

---
+++ elpa/treemacs-evil-20190417.525/treemacs-evil-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-evil" "20190417.525" "Evil mode integration for treemacs" '((evil "1.2.12") (treemacs "0.0")) :commit "242b3f265949d729726d6c4c999625e6ee7d6da6" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
